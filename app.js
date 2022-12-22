c699dc6414c1b05b21c7d62ad4a9e7cff816946e
Harsh Jain <harsh.jain@srrintl.com>
2022-12-22 19:41:51 +0530

    domain setup with db connection

--- app.js
+++ app.js
@@ -1,81 +1,65 @@
-var express         =require("express"),
-    port            =8000,
-    app             =express(),
-    domain          =require('domain'),
-    mongoose        =require("mongoose"),
-    session         =require("express-session"),
-    MongoStore = require("connect-mongodb-session")(session),
-    fs = require('fs'),
-    dotenv =require('dotenv').config(),database,
-    connectionString=process.env.db2;
+var express = require("express"),
+  port = 8000,
+  app = express(),
+  mongoose = require("mongoose"),
+  session = require("express-session"),
+  MongoStore = require("connect-mongodb-session")(session),
+  fs = require('fs'),
+  dotenv = require('dotenv').config(), db, serverDomain,
+  connectionString = process.env.db2;
 
-mongoose.createConnection(connectionString,{
-    domainsEnabled: true,
+//session create
+var store = new MongoStore({
+  uri: connectionString,
+  databaseName: 'connect_mongodb_session_test',
+  collection: 'sessions',
+  connectionOptions: {
+    domainsEnabled: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     ssl: true,
     sslValidate: false,
     sslCA: `${__dirname}/certs/tlsca.pem`,
     sslKey: fs.readFileSync(`${__dirname}/certs/tlsckf.pem`),
-  }).then(function(connection) {
-    this.connection = connection;
-    this.connection.connectionUri = connectionString;
-    this.connection.on('error', function(err){ console.log(err); });
-    this.connection.once('error', function(err){ console.log(err); }); 
-    this.updatedAt = new Date();
-    this._models = {};
-    this.connection.db.command({ buildInfo: 1 }, (error, info) => {
-      if (error) {
-        console.log(error);
-      } 
-      this.mongoVersion = info.version;
-      database = this;
-      console.log("success");
-    });
-  }.bind(this));
- 
-var store = new MongoStore({
-    uri: connectionString,
-    collection: 'sessions',
-    connectionOptions: {
-      domainsEnabled: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      ssl: true,
-      sslValidate: false,
-      sslCA: `${__dirname}/certs/tlsca.pem`,
-      sslKey: fs.readFileSync(`${__dirname}/certs/tlsckf.pem`),
-    }
-  });
-store.on('error', function(error) {
-    console.log(error);
-  });
-  
+  }
+});
+store.on('error', function (error) {
+  console.log(error);
+});
 app.use(session({
-    secret:"sessionSecret",
-    store: store,
-      cookie: {
-        maxAge: 1000
-      },
-      resave: true,
-      saveUninitialized: true,
-    }),
-  );
+  secret: "sessionSecret",
+  store: store,
+  cookie: {
+    maxAge: 1000 * 60 * 60
+  },
+  resave: true,
+  saveUninitialized: true,
+}),
+);
 
-app.use(function(req,res,next){
-    var serverDomain = domain.create();
-    serverDomain.session = req.session;
-    serverDomain.run(function () {
-        next()
-      });
+//creating domain and creating db connection
+app.use(function (req, res, next) {
+  serverDomain = require('domain').create();
+  db = mongoose.createConnection(connectionString, { domainsEnabled: true, useNewUrlParser: true, useUnifiedTopology: true, ssl: true, sslValidate: false, sslCA: `${__dirname}/certs/tlsca.pem`, sslKey: fs.readFileSync(`${__dirname}/certs/tlsckf.pem`) });
+  db.on("error", function (err) { console.log(err) });
+  db.on("connected", function () { console.log("connection connected") });
+  serverDomain.session = req.session;
+  serverDomain.run(function () {
+    process.domain.add(req)
+    process.domain.add(res)
+    process.domain.enter()
+    process.domain.req = req;
+    process.domain.res = res;
+    process.domain.session = req.session;
+    next()
+  });
 })
 
-//HOME
-app.get('/', function(req, res) {
-  res.send('Hello <br>'+ JSON.stringify(req.session.id) + '<br>' + JSON.stringify(req.session));
+//session
+app.get('/', function (req, res) {
+  res.send('Hello <br>' + JSON.stringify(req.session.id) + '<br>' + JSON.stringify(req.session));
 });
 
-app.listen(port,function()
-{
-    console.log("http://localhost:"+port+"/");
+app.listen(port, function () {
+  console.log("http://localhost:" + port + "/");
 });
